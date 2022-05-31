bcdcccbde6c9f56f8fcab324af66daaf40671321
nickichoo <nicki9656@gmail.com>
2022-05-31 15:38:30 +0800

    first message after pat

---
+++ ISGroupSchoolInfo/ISGroupSchoolInfo.cpp
@@ -0,0 +1,12 @@
+// ISGroupSchoolInfo.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "First message from Nicki" << endl;
+    cout << "After pat";
+}
+
